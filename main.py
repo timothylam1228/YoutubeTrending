6fe84380704daf0723d5d9a344a756789de4198a
kalong-leung <leonellus0123@gmail.com>
2023-01-16 13:41:49 +0800

    Added makedir, ENV

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-from PIL import Image, ImageDraw, ImageFont
+import os
 import numpy as np
 import json
 import urllib.request
 from data import Data
 from processor import Processor
 
+SAVE_FOLDER = 'result'
+
 
 def main():
+    if not os.path.exists(SAVE_FOLDER):
+        os.mkdir(SAVE_FOLDER)
     f = open("data.json", encoding="utf-8")
     json_data = json.load(f)
     data = Data(json_data)
